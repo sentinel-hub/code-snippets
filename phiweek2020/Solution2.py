e8b756eb4ebc4ad7066a41f3b552ab04ba4a9593
mlamare <maxim.lamare@sinergise.com>
2020-09-29 14:21:28 +0200

    Update solution files for phiweek tutorial

--- phiweek2020/Solution2.py
+++ phiweek2020/Solution2.py
@@ -1,21 +1,6 @@
-evalscript_landsat_true_color_improved = """
-//VERSION=3
-function setup() {
-  return {
-    input: ["B02","B03", "B04", "B08"],
-    output: { bands: 3 }
-  };
-}
-
-function evaluatePixel(samples) {
-  
-  // Use weighted arithmetic average for pan-sharpening
-  let s2PanR3 = (samples.B04 + samples.B03 + samples.B02) / 3
-  let s2ratioWR3 = s2PanR3 / samples.B08
-  let gain = 2.5
-  return [gain * samples.B04 * s2ratioWR3, gain * samples.B03 * s2ratioWR3, gain * samples.B02 * s2ratioWR3]
-}
-"""
+# The same Evalscript works for Landsat-8 and Sentinel-2
+# Both sensors have RGB bands named: B04, B03, B02
+evalscript_landsat_true_color = evalscript_true_color
 
 # Adjust the image size for the change in resolution
-aoi_size_landsat = bbox_to_dimensions(aoi, resolution=15)
+aoi_size_landsat = bbox_to_dimensions(aoi, resolution=30)
